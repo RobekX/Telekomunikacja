ffe4df34332ac60a9f6fb5d43a6241508a5a4abe
Raqiet <203942@edu.p.lodz.pl>
2017-06-17 16:29:40 +0200

    Comments to z3

--- TK_Z3/ConsoleApplication3/Program.cs
+++ TK_Z3/ConsoleApplication3/Program.cs
@@ -6,57 +6,64 @@ namespace ConsoleApplication3
 {
     public class Program
     {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args">Not used.</param>
         public static void Main(string[] args)
         {
             Console.WriteLine("ATChat");
             Console.WriteLine("Coded by Dawid Michałowski and Robert Radczyc");
             Console.WriteLine("Avaiable ports:");
-            foreach(var x in SerialPort.GetPortNames())
+            foreach(var x in SerialPort.GetPortNames()) //pobranie wszystkich dostępnych portów
             {
                 Console.WriteLine(x);
             }
             Console.Write("Provide port: ");
             var input = Console.ReadLine();
-            foreach (var x in SerialPort.GetPortNames())
+            foreach (var x in SerialPort.GetPortNames()) //przeszukanie czy wprowadzony port znajduje sie wsrod dostepnych portow
             {
                 if(x == input)
                 {
                     Console.Clear();
                     Console.WriteLine($"Connected to {input}.");
-                    PortLoop(input);
+                    PortLoop(input); //uruchomienie metody z parametrem wybranego portu
                 }
             }
             Console.WriteLine("Nie znaleziono portu.");
             Console.ReadLine();
            
         }
-
+        /// <summary>
+        /// Metoda odpowiedzialna za zczytywanie wiadomości użytkownika i wysyłanie ich do portu
+        /// </summary>
+        /// <param name="com">Port com.</param>
         public static void PortLoop(string com)
         {
-            SerialPort _serialPort = new SerialPort(com, 9600);
-            _serialPort.DtrEnable = true;
-            _serialPort.RtsEnable = true;
-            _serialPort.DataBits = 8;
+            SerialPort _serialPort = new SerialPort(com, 9600); //utworzenie portu (comport, baudrate)
+            _serialPort.DtrEnable = true; //Data Terminal Ready
+            _serialPort.RtsEnable = true; //Request to send
+            _serialPort.DataBits = 8; //rozmiar elementu
             _serialPort.Handshake = Handshake.None;
             _serialPort.Parity = Parity.None;
-            _serialPort.StopBits = StopBits.One;
+            _serialPort.StopBits = StopBits.One; //bit stopu = 1
             _serialPort.NewLine = System.Environment.NewLine;
-            _serialPort.DataReceived += (s, e) => {
+            _serialPort.DataReceived += (s, e) => { //Event odebrania danych
                 SerialPort sp = (SerialPort)s;
-                byte[] packOfBytes = new byte[sp.BytesToRead];
-                Console.Write(Encoding.ASCII.GetString(packOfBytes, 0, sp.Read(packOfBytes, 0, packOfBytes.Length)));
+                byte[] packOfBytes = new byte[sp.BytesToRead]; //odczytana wiadomosc do tablicy bajtow
+                Console.Write(Encoding.ASCII.GetString(packOfBytes, 0, sp.Read(packOfBytes, 0, packOfBytes.Length))); //konwersja bajtow na ascii
             };
-            _serialPort.Open();
+            _serialPort.Open(); //otwarcie portu
 
-            while (true)
+            while (true) //pętla zczytujaca dane od uzytkownika
             {
                 var msg = Console.ReadLine();
                 if (msg == "quit")
                 {
-                    _serialPort.Close();
-                    System.Environment.Exit(1);
+                    _serialPort.Close(); //jesli zostanie wpisany quit to zamknij port...
+                    System.Environment.Exit(1); // ...i program
                 }
-                _serialPort.WriteLine(msg);
+                _serialPort.WriteLine(msg); //wyslij wiadomosc do portu
             }
         }
     }
